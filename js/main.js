52b6bd7de563cda9aadcd1d7699761aebb965a50
strizich <strizich@me.com>
2018-01-18 11:52:29 -0600

    layout updates
    Added reservation-setup.html template for step1 of reservations

--- js/main.js
+++ js/main.js
@@ -1,44 +1,59 @@
-var myScroll;
-var position;
+$(function() {
+    //caches a jQuery object containing the header element
+    var body = $(".page-wrapper");
+    $('#wrapper').scroll(function() {
+        var scroll = $('#wrapper').scrollTop();
+        if (scroll >= 68) {
+            body.removeClass('not-scrolled').addClass("scrolled");
+        } else {
+            body.removeClass("scrolled").addClass('not-scrolled');
+        }
+    });
+});
 
-function loaded () {
-	snap = document.getElementById('snap');
-	
-	bodyScroll = new IScroll('#wrapper', { 
-		probeType:  3,
-	    mouseWheel: true,
-	    scrollbars: 'custom',
-	    bounce: true,
-	    keyBindings: true,
-	    invertWheelDirection: false,
-	    momentum: true,
-	    fadeScrollbars: true,
-	    interactiveScrollbars: true,
-	    resizeScrollbars: true,
-	    shrinkScrollbars: false,
-	    click: false,
-	    deceleration: 0.001,
-	    preventDefaultException: { tagName:/.*/ }  
-	});
 
-	bodyScroll.on('scroll', function () {
-	    if ( this.y <= -75 ) {
-	        $('body').addClass('scrolled');
-	    } else {
-	    	$('body').removeClass('scrolled');
-	    }
-	    if ( this.directionY == 1){
-	    	$('body').addClass('scrollDown');
-	    	$('body').removeClass('scrollUp');
-	    }
-	    else if ( this.directionY == -1){
-	    	$('body').addClass('scrollUp');
-	    	$('body').removeClass('scrollDown');
-	    }
-	});
 
-	   
-}
+//var myScroll;
+//var position;
+
+//function loaded () {
+//	snap = document.getElementById('snap');
+//	
+//	bodyScroll = new IScroll('#wrapper', { 
+//		probeType:  3,
+//	    mouseWheel: true,
+//	    scrollbars: 'custom',
+//	    bounce: true,
+//	    keyBindings: true,
+//	    invertWheelDirection: false,
+//	    momentum: true,
+//	    fadeScrollbars: true,
+//	    interactiveScrollbars: true,
+//	    resizeScrollbars: true,
+//	    shrinkScrollbars: false,
+//	    click: false,
+//	    deceleration: 0.001,
+//	    preventDefaultException: { tagName:/.*/ }  
+//	});
+//
+//	bodyScroll.on('scroll', function () {
+//	    if ( this.y <= -75 ) {
+//	        $('body').addClass('scrolled');
+//	    } else {
+//	    	$('body').removeClass('scrolled');
+//	    }
+//	    if ( this.directionY == 1){
+//	    	$('body').addClass('scrollDown');
+//	    	$('body').removeClass('scrollUp');
+//	    }
+//	    else if ( this.directionY == -1){
+//	    	$('body').addClass('scrollUp');
+//	    	$('body').removeClass('scrollDown');
+//	    }
+//	});
+//
+//	   
+//}
 
 
 $(document).ready(function(){
@@ -70,23 +85,4 @@ $(document).ready(function(){
 	    }
     }
 });
-    function callback(event) {
-        var pages = event.page.count;     // Number of pages
-        var currentPage = event.page.index; 
-        var bodyScroll = new IScroll('#wrapper');
-        if (currentPage == 0){
-          bodyScroll.scrollToElement('.cal__week--1', 8000);
-            console.log('okay');
-        } else if (currentPage == 1){
-        	bodyScroll.scrollToElement('.cal__week--2' , 8000);
-        }
-        else if (currentPage == 2){
-        	bodyScroll.scrollToElement('.cal__week--3' , 8000);
-        }
-        else if (currentPage == 3){
-        	bodyScroll.scrollToElement('.cal__week--4' , 8000);
-        }
-        
-    }
 });
-document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
